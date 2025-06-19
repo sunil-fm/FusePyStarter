5bc710c322dc1059a6ff76a6dd897ae59da4df53
kamal <kamalgodar9244@gmail.com>
2025-06-19 12:57:40 +0545

    chore: selection of correct secret file from selected env

--- config/config.py
+++ config/config.py
@@ -5,7 +5,5 @@ settings = Dynaconf(
     environments=True,  # enables [dev], [stage], [prod]
     envvar_prefix="DYNACONF", # sets the prefix for overriding values with environment variables (via export)
     load_dotenv = True, # loads .env file if exists
-    settings_files=['settings.toml', "secrets/.secrets.toml"], # supports any order (the later files can override earlier ones).
+    settings_files=['settings.toml', "secrets/.secrets.toml", f'secrets/.secrets.{os.getenv("ENV_FOR_DYNACONF")}.toml'], # supports any order (the later files can override earlier ones).
 )
-
-# , f'secrets/.secrets.{os.getenv("ENV_FOR_DYNACONF")}.toml'
