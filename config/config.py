514a7917884621ee224fec9efe883b21d263a3c7
ghimiresunil <sunilghimire64@gmail.com>
2025-06-24 14:24:13 +0545

    chore(config,docs): remove deprecated configuration and related documentation files

--- config/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dynaconf import Dynaconf
-
-settings = Dynaconf(
-    environments=True,  # enables [dev], [stage], [prod]
-    envvar_prefix="DYNACONF", # sets the prefix for overriding values with environment variables (via export)
-    load_dotenv = True, # loads .env file if exists
-    settings_files=['settings.toml', "secrets/.secrets.toml", f'secrets/.secrets.{os.getenv("ENV_FOR_DYNACONF")}.toml'], # supports any order (the later files can override earlier ones).
-)
