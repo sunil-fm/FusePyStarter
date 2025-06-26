f47cdd41d252315706112e724cbf21ed577fbc03
ghimiresunil <sunilghimire64@gmail.com>
2025-06-26 07:36:00 +0545

    chore (__init__): packaging  AppLogger

--- src/decorators/__init__.py
+++ src/decorators/__init__.py
@@ -1,1 +1,8 @@
 """Initialize decorators package and expose public logging utilities."""
+
+from src.decorators.logging_service import AppLogger
+
+app_logger = AppLogger()
+logger = app_logger.get_logger()
+
+__all__ = ["logger"]
