fd8f2b0a2a0b3c3ab5215386b570be4d0f80aaa1
sunil-fm <sunil.ghimire@fusemachines.com>
2025-07-08 19:31:16 +0545

    fix(conf): update the path for the src

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,26 @@
 """Sphinx configuration for building project documentation."""
 
-import os
 import sys
 from pathlib import Path
 
 import toml
 
-sys.path.insert(0, os.path.abspath("../src/"))
+# -- Path setup --------------------------------------------------------------
+
+# Dynamically determine the project root and src directory
+current_dir = Path(__file__).resolve().parent
+project_root = current_dir.parent
+src_path = project_root / "src"
+
+
+# Add src to sys.path so packages like `temperature` can be found
+sys.path.insert(0, str(src_path))
+
+
+# -- Project metadata from pyproject.toml ------------------------------------
 
 
 def _get_project_meta():
-    project_root = Path(__file__).resolve().parent.parent
     return toml.load(project_root / "pyproject.toml")["project"]
 
 
@@ -18,13 +28,11 @@ pkg_meta = _get_project_meta()
 project = pkg_meta["name"]
 copyright = "2025, Sunil Ghimire"
 author = pkg_meta["authors"][0]["name"]
-
 version = pkg_meta["version"]
-release = version
+release = version  # Full version
 
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -39,16 +47,13 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# Include TODOs in the output if set to True
+todo_include_todos = False
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
-
-# If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = False
-
-# The name of the Pygments (syntax highlighting) style to use.
+# Use default syntax highlighting
 pygments_style = "sphinx"
