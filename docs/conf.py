76b2aea8e84fc274fe815cea10d0c513c464abef
sunil-fm <sunil.ghimire@fusemachines.com>
2025-07-08 16:54:00 +0545

    revert(conf): to view the src content in github pages

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,8 @@
 """Sphinx configuration for building project documentation."""
 
-import os
-import sys
 from pathlib import Path
 
 import toml
-
-# Add src directory to Python path for autodoc
-sys.path.insert(0, os.path.abspath("../src"))
 
 
 def _get_project_meta():
@@ -23,6 +18,7 @@ author = pkg_meta["authors"][0]["name"]
 version = pkg_meta["version"]
 release = version
 
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
@@ -34,69 +30,21 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
-    "sphinx.ext.intersphinx",
 ]
-
-# Autodoc configuration
-autodoc_default_options = {
-    "members": True,
-    "member-order": "bysource",
-    "special-members": "__init__",
-    "undoc-members": True,
-    "exclude-members": "__weakref__",
-}
-autodoc_mock_imports = []  # Add any problematic imports here
-
-# Napoleon settings for Google-style docstrings
-napoleon_google_docstring = True
-napoleon_numpy_docstring = False
-napoleon_include_init_with_doc = True
-napoleon_include_private_with_doc = False
-napoleon_include_special_with_doc = True
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-# The suffix of source filenames.
-source_suffix = [".rst", ".md"]
-
-# The master toctree document.
-master_doc = "index"
 
 # -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
-# Theme options
-html_theme_options = {
-    "navigation_depth": 4,
-    "collapse_navigation": False,
-    "sticky_navigation": True,
-    "includehidden": True,
-    "titles_only": False,
-}
 
-# If true, links to the reST sources are added to the pages.
-html_show_sourcelink = True
-
-# If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-html_show_sphinx = False
-
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-html_show_copyright = True
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = f"{project}doc"
-
-# -- Options for todo extension ----------------------------------------------
+# If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
-# -- Options for Pygments ---------------------------------------------------
+# The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
-
-# -- Options for intersphinx extension ---------------------------------------
-intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
-
-# -- Options for GitHub Pages -----------------------------------------------
-# Ensure links work properly on GitHub Pages
-html_baseurl = f"/{project}/"
