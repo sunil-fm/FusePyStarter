361a9d92efbe99f83bfedf415d260dc83d69380b
sunil-fm <sunil.ghimire@fusemachines.com>
2025-07-08 18:07:32 +0545

    build(gh-pages and conf): build src directory to show the content

--- docs/conf.py
+++ docs/conf.py
@@ -2,29 +2,53 @@
 
 import os
 import sys
+from pathlib import Path
+
+import toml
 
 sys.path.insert(0, os.path.abspath("../src"))
 
-project = "Documentation"
+
+def _get_project_meta():
+    project_root = Path(__file__).resolve().parent.parent
+    return toml.load(project_root / "pyproject.toml")["project"]
+
+
+pkg_meta = _get_project_meta()
+project = pkg_meta["name"]
 copyright = "2025, Sunil Ghimire"
-author = "Sunil Ghimire"
-release = "0.0.1"
+author = pkg_meta["authors"][0]["name"]
+
+version = pkg_meta["version"]
+release = version
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",  # For Google and NumPy style docstrings
-    "sphinx.ext.viewcode",  # Optional: to include links to source code
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = []
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
