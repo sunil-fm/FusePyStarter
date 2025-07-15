a6d7f54b1fb2dfff8c4dea4d4c87020ddd963e17
sunil-fm <sunil.ghimire@fusemachines.com>
2025-07-15 17:57:31 +0545

    chore(config): Dynaconf config to load settings.toml and .secrets.toml based on configs directory

--- configs/config.py
+++ configs/config.py
@@ -1,11 +1,16 @@
 """Load configuration using Dynaconf with environment and dotenv support."""
 
+from pathlib import Path
+
 from dynaconf import Dynaconf
+
+_BASE_PATH = Path(__file__).parent.parent
+_CONFIG_PATH = _BASE_PATH / "configs"
 
 settings = Dynaconf(
     environments=True,
     envvar_prefix="DYNACONF",
     load_dotenv=True,
-    settings_files=["configs/settings.toml", "configs/.secrets.toml"],
+    settings_files=[_CONFIG_PATH / "settings.toml", _CONFIG_PATH / ".secrets.toml"],
     env_switcher="ENV_FOR_DYNACONF",
 )
